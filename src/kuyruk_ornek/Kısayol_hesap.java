1058dd546bb39ee6ec85999e819c94056e046f9d
Bekir Ersever <47231687+bekir1184@users.noreply.github.com>
2019-11-21 08:34:50 +0300

    Örnekler eklendi

--- src/kuyruk_ornek/Kısayol_hesap.java
+++ src/kuyruk_ornek/Kısayol_hesap.java
@@ -7,9 +7,8 @@ public class Kısayol_hesap {
     Konum baslangic;
     Konum hedef;
     ArrayList<Kuyruk> kuyruklar;
-    int arrayListS=0;
-    int kuyrukS=0;
-    int konumS=0;
+    int arrayListS = 0;
+    int konumS = 0;
 
     public Kısayol_hesap(Konum baslangic, Konum hedef) {
         this.baslangic = baslangic;
@@ -21,6 +20,10 @@ public class Kısayol_hesap {
         
 
     }
+    public Kısayol_hesap(Konum hedef){
+        this.baslangic=new Konum(0,0);
+        this.hedef=hedef;
+    }
 
     public int kısayolHesapla() {
         int adimSayisi = 0;
@@ -29,12 +32,12 @@ public class Kısayol_hesap {
         while (!bitti) {
             int kuyruksayisi = kuyruklar.size();
             int sayac = 0;
-            
+
             while (sayac < kuyruksayisi) {
-                
+
                 konumS = 0;
                 while (!kuyruklar.get(sayac).bosMu()) {
-                    
+
                     Konum temp = kuyruklar.get(sayac).cikar();
 
                     if (temp.pozX == hedef.pozX && temp.pozY == hedef.pozY) {
@@ -44,23 +47,44 @@ public class Kısayol_hesap {
 
                         kuyruklar.add(konumAyarla(temp));
                     }
-                    System.out.println("ArrayList : "+arrayListS+" Kuyruk :"+sayac+" Konum : "+konumS+" -- >temp x :" + temp.pozX + " temp y: " + temp.pozY + " = bitti mi ? ->" + bitti);
-                    
+                    System.out.println("ArrayList : " + arrayListS + " Kuyruk :" + sayac + " Konum : " + konumS + " -- >temp x :" + temp.pozX + " temp y: " + temp.pozY + " = bitti mi ? ->" + bitti);
+
                     konumS++;
                 }
-                kuyrukS++;
+
                 sayac++;
             }
             System.out.println("Adım sayısı arttı");
             adimSayisi++;
-            kuyrukS=0;
             arrayListS++;
         }
 
         return 0;
     }
-    
-    
+    public int alinanMesafe(){
+        int adimSayisi=0;
+        while(true){
+            System.out.println("baslangic x:"+baslangic.pozX+" baslangic y: "+baslangic.pozY+" hedef x:"+hedef.pozX+" hedef y: "+hedef.pozY);
+            if(baslangic.pozX > 7){
+                baslangic.pozY=baslangic.pozY+1;
+                baslangic.pozX=0;
+            }
+            if(baslangic.pozY > 7){
+                break;
+                
+            }
+            if(baslangic.pozX == hedef.pozX && baslangic.pozY== hedef.pozY){
+                break;
+                
+            }
+            baslangic.pozX=baslangic.pozX+1;
+            adimSayisi++;
+        }
+        
+        
+        
+        return adimSayisi;
+    }
 
     public Kuyruk konumAyarla(Konum konum) {
         Kuyruk konumlar = new Kuyruk(8);
@@ -80,12 +104,18 @@ public class Kısayol_hesap {
         konumlar.ekle(yedinciDurum);
         Konum sekizinciDurum = new Konum(konum.pozX + 2, konum.pozY - 1);
         konumlar.ekle(sekizinciDurum);
-
+        
+        konumlar.ekle(new Konum(konum.pozX+1,konum.pozY+1));
+        konumlar.ekle(new Konum(konum.pozX-1,konum.pozY-1));
+        
         return konumlar;
     }
 
     public static void main(String[] args) {
-        Kısayol_hesap k = new Kısayol_hesap(new Konum(7, 0), new Konum(0, 7));
-        System.out.println(k.kısayolHesapla());
+        Kısayol_hesap k = new Kısayol_hesap(new Konum(0, 5), new Konum(5, 2));
+        //System.out.println(k.kısayolHesapla());
+        Kısayol_hesap quiz = new Kısayol_hesap(new Konum(2,2));
+       System.out.println(quiz.alinanMesafe());
+    
     }
 }
